4cab9b45642eb1e6b495d484422dd05a8419c716
ducdat0402 <leducdat0402@gmail.com>
2026-09-23 17:22:18 +0700

    add is_sent

--- UniCore.Backend/UniCore.Infrastructure/Database/ModelCreating/AnnouncementStudentModelCreating.cs
+++ UniCore.Backend/UniCore.Infrastructure/Database/ModelCreating/AnnouncementStudentModelCreating.cs
@@ -13,16 +13,29 @@ namespace UniCore.Infrastructure.Database.ModelCreating
 
                 entity.HasKey(e => e.Id);
 
-                entity.Property(e => e.Id).HasColumnName("id").HasMaxLength(50);
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasMaxLength(50)
+                    .ValueGeneratedOnAdd()
+                    .HasDefaultValueSql("dbo.fn_GenerateUUIDv7()");
                 entity.Property(e => e.AnnouncementId).HasColumnName("announcement_id").IsRequired().HasMaxLength(50);
                 entity.Property(e => e.StudentId).HasColumnName("student_id").IsRequired().HasMaxLength(50);
                 entity.Property(e => e.ViewedAt).HasColumnName("viewed_at");
                 entity.Property(e => e.AcknowledgedAt).HasColumnName("acknowledged_at");
-                entity.Property(e => e.IsSent).HasColumnName("is_sent").HasDefaultValue(false);
+                entity.Property(e => e.IsSent)
+                    .HasColumnName("is_sent")
+                    .HasDefaultValue(false);
                 entity.Property(e => e.CreatedAt).HasColumnName("created_at").HasDefaultValueSql("sysutcdatetime()");
 
-                entity.HasIndex(e => new { e.AnnouncementId, e.StudentId }).IsUnique();
-                entity.HasIndex(e => e.StudentId);
+                entity.HasIndex(e => new { e.AnnouncementId, e.StudentId })
+                    .IsUnique()
+                    .HasDatabaseName("UQ_announcement_students");
+
+                entity.HasIndex(e => e.StudentId)
+                    .HasDatabaseName("IX_announcement_students_student");
+
+                entity.HasIndex(e => new { e.AnnouncementId, e.AcknowledgedAt })
+                    .HasDatabaseName("IX_announcement_students_ack");
 
                 entity.HasOne(e => e.Announcement)
                     .WithMany(a => a.AnnouncementStudents)
--- UniCore.Database/dbo/Tables/announcement_students.sql
+++ UniCore.Database/dbo/Tables/announcement_students.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[announcement_students] (
     [student_id]      VARCHAR (50)  NOT NULL,
     [viewed_at]       DATETIME2 (7) NULL,
     [acknowledged_at] DATETIME2 (7) NULL,
-    [is_sent]         BIT           DEFAULT ((0)) NOT NULL,
+    [is_sent]         BIT           CONSTRAINT [DF_announcement_students_is_sent] DEFAULT ((0)) NOT NULL,
     [created_at]      DATETIME2 (7) DEFAULT (sysutcdatetime()) NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC),
     CONSTRAINT [UQ_announcement_students] UNIQUE ([announcement_id], [student_id]),
@@ -15,4 +15,8 @@ GO
 
 CREATE NONCLUSTERED INDEX [IX_announcement_students_student]
     ON [dbo].[announcement_students] ([student_id] ASC);
+GO
+
+CREATE NONCLUSTERED INDEX [IX_announcement_students_ack]
+    ON [dbo].[announcement_students] ([announcement_id] ASC, [acknowledged_at] ASC);
 GO
